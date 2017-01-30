db641b0a90aa10e364ffc920df892c905310d648
meghraj7471 <meghraj7471@gmail.com>
2017-01-31 00:06:52 +0530

    Add files via upload

---
+++ Demo.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Demo.aspx.cs" Inherits="EducationCommunity.Demo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    Rows:
+    <asp:TextBox ID="txtRows" runat="server" />&nbsp;
+    Cols:
+    <asp:TextBox ID="txtCols" runat="server" />
+    <br /><br />
+    <asp:CheckBox ID="chkBorder" runat="server"
+         Text="Put Border Around Cells" />
+    <br /><br />
+    <asp:Button ID="cmdCreate" OnClick="cmdCreate_Click" runat="server"
+     Text="Create" />
+    <br /><br />
+    <asp:Table ID="tbl" runat="server" />
+  </div>
+    </form>
+</body>
+</html>
---
+++ Demo.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace EducationCommunity
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            tbl.BorderStyle = BorderStyle.None;
+            tbl.BorderWidth = Unit.Pixel(1);
+        }
+
+     
+        protected void cmdCreate_Click(object sender, EventArgs e)
+        {
+
+            int rows = Int32.Parse(txtRows.Text);
+            int cols = Int32.Parse(txtCols.Text);
+
+            for (int i = 0; i < rows; i++)
+            {
+                TableRow rowNew = new TableRow();
+                tbl.Controls.Add(rowNew);
+                for (int j = 0; j < cols; j++)
+                {
+                    TableCell cellNew = new TableCell();
+                    Label lblNew = new Label();
+                    lblNew.Text = "(" + i.ToString() + "," + j.ToString() + ")<br />";
+                    cellNew.Controls.Add(lblNew);
+
+
+                    cellNew.BorderStyle = BorderStyle.None;
+                    cellNew.BorderWidth = Unit.Pixel(1);
+
+
+                    rowNew.Controls.Add(cellNew);
+                }
+            }
+        }
+    }
+}
